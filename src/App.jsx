ee7b3217b36234c0cf47ee1f78d3600d7c18021d
4lPeReZ <alvaropr05@hotmail.com>
2023-01-22 17:59:13 +0100

    Add Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import "tailwindcss/tailwind.css"
 import Characters from './components/characters'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="flex flex-wrap">
       <Characters/>
     </div>
   )
--- src/components/characters.jsx
+++ src/components/characters.jsx
@@ -8,31 +8,38 @@ function Characters() {
   //   const privateKeyAPI = "076e7ded8107fb235ff5e77324ed1d263353ca2e";
   //   const Key = "1076e7ded8107fb235ff5e77324ed1d263353ca2eb7eca3b33098c5b2c9564b385354f8c0";
   const md5Key = "6a15a6fd426a36a29620438c8873103a";
-  const urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey="+publicKeyAPI+"&hash=" +md5Key;
-
+  const urlAPI =
+    "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=" +
+    publicKeyAPI +
+    "&hash=" +
+    md5Key;
 
   useEffect(() => {
-    const fetch = async()=>{
-      const res = await axios.get(urlAPI)
+    const fetch = async () => {
+      const res = await axios.get(urlAPI);
       setCharactersData(res.data.data.results);
-    }
+    };
     fetch();
-  },[urlAPI])
+  }, [urlAPI]);
 
   //Cada vez que se recarga la pagina mediante el navegador se rompe la llamada a la API o la respuesta.
-  
-// console.log(charactersData[0].id)
+
+  // console.log(charactersData[0].id)
+
+  // Mapeo de las imagenes de los personajes de la API
+
   return (
-    <div>
-        {/* {charactersData[1].thumbnail.path}.{charactersData[1].thumbnail.extension} */}
-        {charactersData[1].name}
-
-        {/* <p></p> */}
-        
-        <img src={`${charactersData[1].thumbnail.path}.${charactersData[1].thumbnail.extension}`} alt="" />
-
-        {/* {charactersData[1].name} */}
-        
+    <div className="flex flex-wrap">
+      {charactersData.map((character, index) => (
+        <div key={index} className="w-3/4">
+          <img
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.name}
+            className="w-full h-auto rounded-md"
+          />
+          <p className="text-center text-lg font-medium">{character.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
